e70be05fea862576decb43bf5227b1e4a1e6aaa3
NamTran <tcnam2000@gmail.com>
2021-12-19 15:09:34 +0700

    first commit, crawler of laodong.vn

---
+++ api.py
@@ -0,0 +1,5 @@
+import pymongo
+from pymongo import MongoClient
+
+cluster =MongoClient("mongodb+srv://trancongnam:G2wL8HxyMgmu4aIo@cluster0.l8x9e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+
