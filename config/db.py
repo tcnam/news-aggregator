5a486997567e028de57d4300ac2b7a5180f7dfec
NamTran <tcnam2000@gmail.com>
2021-12-20 16:19:25 +0700

    delete unnecessary file

--- config/db.py
+++
@@ -1,6 +0,0 @@
-from pymongo import MongoClient
-
-
-cluster =MongoClient("mongodb+srv://<username>:<password>@cluster0.l8x9e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-db=cluster['news_aggregator']
-collection=db['news']
