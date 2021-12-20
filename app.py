2e47cc21f8b513cbc6142a52f9c797c88ff291d1
NamTran <tcnam2000@gmail.com>
2021-12-20 21:47:19 +0700

    test for deploying heroku 2

--- app.py
+++ app.py
@@ -5,13 +5,24 @@ import pymongo
 connection_url = 'mongodb+srv://trancongnam:G2wL8HxyMgmu4aIo@cluster0.l8x9e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 app = Flask(__name__)
 client = pymongo.MongoClient(connection_url)
-  
+
 # Database
 Database = client.get_database('news_aggregator')
 # Table
 NewsTable = Database.news
-
-@app.route('/find/', methods=['GET'])
+def CategoryNormalize(category):
+    switcher={
+        'yte':'Y tế',
+        'xahoi':'Xã hội',
+        'video':'Video',
+        'moitruong':'Môi trường',
+        'kinhdoanh':'Kinh doanh',
+        'phapluat':'Pháp luật',
+        'thegioi':'Thế giới',
+        'giaoduc':'Giáo dục',
+    }
+    return switcher.get(category)
+@app.route('/news/', methods=['GET'])
 def findAll():
     query = NewsTable.find()
     output = {}
@@ -22,5 +33,18 @@ def findAll():
         i += 1
     return jsonify(output)
 
+@app.route('/news/<categoryvalue>/',methods=['GET'])
+def findNewsBasedOnCategory(categoryvalue):
+    catevalue=CategoryNormalize(categoryvalue)
+    output = {}
+    i = 0
+    for result in NewsTable.find({'category':catevalue}):
+        output[i] = result
+        output[i].pop('_id')
+        i += 1
+    return jsonify(output)
+    
+    
 if __name__ == '__main__':
     app.run(debug=True)
+
