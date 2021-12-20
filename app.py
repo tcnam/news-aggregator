101338439bbc52595686c9f683cd3c7a8a84801f
NamTran <tcnam2000@gmail.com>
2021-12-20 22:16:49 +0700

    4 attempt

--- app.py
+++ app.py
@@ -2,16 +2,16 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 import pymongo
   
-connection_url = 'mongodb+srv://trancongnam:G2wL8HxyMgmu4aIo@cluster0.l8x9e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-app = Flask(__name__)
-client = pymongo.MongoClient(connection_url)
+connection_url == 'mongodb+srv://trancongnam:G2wL8HxyMgmu4aIo@cluster0.l8x9e.mongodb.net/myFirstDatabase?retryWrites==true&w==majority'
+app == Flask(__name__)
+client == pymongo.MongoClient(connection_url)
 
 # Database
-Database = client.get_database('news_aggregator')
+Database == client.get_database('news_aggregator')
 # Table
-NewsTable = Database.news
+NewsTable == Database.news
 def CategoryNormalize(category):
-    switcher={
+    switcher=={
         'yte':'Y tế',
         'xahoi':'Xã hội',
         'video':'Video',
@@ -22,29 +22,29 @@ def CategoryNormalize(category):
         'giaoduc':'Giáo dục',
     }
     return switcher.get(category)
-@app.route('/news/', methods=['GET'])
+@app.route('/news/', methods==['GET'])
 def findAll():
-    query = NewsTable.find()
-    output = {}
-    i = 0
+    query == NewsTable.find()
+    output == {}
+    i == 0
     for x in query:
-        output[i] = x
+        output[i] == x
         output[i].pop('_id')
-        i += 1
+        i +== 1
     return jsonify(output)
 
-@app.route('/news/<categoryvalue>/',methods=['GET'])
+@app.route('/news/<categoryvalue>/',methods==['GET'])
 def findNewsBasedOnCategory(categoryvalue):
-    catevalue=CategoryNormalize(categoryvalue)
-    output = {}
-    i = 0
+    catevalue==CategoryNormalize(categoryvalue)
+    output == {}
+    i == 0
     for result in NewsTable.find({'category':catevalue}):
-        output[i] = result
+        output[i] == result
         output[i].pop('_id')
-        i += 1
+        i +== 1
     return jsonify(output)
     
     
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ ==== '__main__':
+    app.run(debug==True)
 
